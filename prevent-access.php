e2b1c0b80fa3dae3aa39acdc2b3bb835d87fa0cd
César Ribeiro <chrdesigner@chrdesigner.com>
2016-05-06 21:20:22 -0300

    Update prevent-access.php

--- prevent-access.php
+++ prevent-access.php
@@ -1,6 +1,4 @@
 <?php
-! defined( 'ABSPATH' ) AND exit;
-
 /*
 Plugin Name: Prevent Access to wp-admin
 Version: 0.1
@@ -11,17 +9,24 @@ Plugin URI: https://github.com/chrdesigner/prevent-access
 License: A slug describing license associated with the plugin (usually GPL2)
 */
 
+! defined( 'ABSPATH' ) AND exit;
 
+// Disable Admin Bar specific to user roles
+function disable_admin_bar() {
+   if ( current_user_can( 'customer' ) || current_user_can( 'subscriber' ) ) {
+      add_filter( 'show_admin_bar', '__return_false' );
+   }
+}
+add_action( 'init', 'disable_admin_bar' , 9 );
+
+// Disable specific access to user roles
 function no_admin_access() {
-	
-	add_filter('show_admin_bar', '__return_false');
-
     $redirect = isset( $_SERVER['HTTP_REFERER'] ) ? $_SERVER['HTTP_REFERER'] : home_url( '/' );
-    
     if ( 
-        current_user_can( 'customer' ) OR current_user_can( 'subscriber' )
+        current_user_can( 'customer' ) || current_user_can( 'subscriber' )
     )
-    
-    exit( wp_redirect( $redirect ) );
+    exit( 
+    	wp_redirect( $redirect )
+    );
 }
 add_action( 'admin_init', 'no_admin_access', 100 );
